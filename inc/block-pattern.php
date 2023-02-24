d6c9e6b3ce7d289cf8dd8a18f7ae04835bee4d08
shivam kushwah <kushwah.shivam30@gmail.com>
2023-02-24 13:21:12 +0530

    save it

--- inc/block-pattern.php
+++ inc/block-pattern.php
@@ -1,79 +1,122 @@
 <?php
 /**
- * blur: Block Patterns
+ * Patterns Handler.
  *
  * @since blur 1.0
  */
+
+namespace Blur;
+
+use WP_Block_Pattern_Categories_Registry;
 
 /**
- * Registers block patterns and categories.
+ * Class Block_Patterns
  *
- * @since blur 1.0
- *
- * @return void
+ * @package Blur
  */
-  function blur_register_block_patterns() {
-	$block_pattern_categories = array(
-		'featured' => array( 'label' => __( 'Featured', 'blur' ) ),
-		'footer'   => array( 'label' => __( 'Footers', 'blur' ) ),
-		'header'   => array( 'label' => __( 'Headers', 'blur' ) ),
-		'query'    => array( 'label' => __( 'Query', 'blur' ) ),
-		'pages'    => array( 'label' => __( 'Pages', 'blur' ) ),
-		'blur'    => array( 'label' => __( 'blur', 'blur' ) ),
-	);
+class Block_Patterns {
 
 	/**
-	 * Filters the theme block pattern categories.
+	 * Patterns categories.
 	 *
-	 * @since blur 1.0
-	 *
-	 * @param array[] $block_pattern_categories {
-	 *     An associative array of block pattern categories, keyed by category name.
-	 *
-	 *     @type array[] $properties {
-	 *         An array of block category properties.
-	 *
-	 *         @type string $label A human-readable label for the pattern category.
-	 *     }
-	 * }
+	 * @var array
 	 */
-	$block_pattern_categories = apply_filters( 'blur_block_pattern_categories', $block_pattern_categories );
+	private $categories = array();
+	/**
+	 * The patterns array.
+	 *
+	 * These use the file names without termination inside the `inc/patterns` directory.
+	 *
+	 * @var array
+	 */
+	private $patterns = array();
 
-	foreach ( $block_pattern_categories as $name => $properties ) {
-		if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
-			register_block_pattern_category( $name, $properties );
+	/**
+	 * Block_Patterns constructor.
+	 */
+	public function __construct() {
+		$this->setup_properties();
+
+		add_action( 'init', array( $this, 'run' ) );
+	}
+
+	/**
+	 * Run the class functionality.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$this->register_categories();
+		$this->register_patterns();
+	}
+
+	/**
+	 * Setup class properties.
+	 *
+	 * @return void
+	 */
+	private function setup_properties() {
+
+		$categories = array(
+			'blur' => array( 'label' => __( 'Blur Patterns', 'blur' ) ),
+		);
+
+		$patterns = array(
+			'hero-main',
+			'three-col-banner',
+			'recent-product',
+			'banner',
+			'top-rated-product',
+			'aboutus',
+			'service',
+			'primary-sidebar',
+			'sidebar-products',
+			'footer-default'
+		);
+
+		$this->categories = apply_filters( 'blur_block_patterns_categories', $categories );
+		$this->patterns   = apply_filters( 'blur_block_patterns', $patterns );
+	}
+
+	/**
+	 * Register block patterns categories.
+	 *
+	 * @return void
+	 */
+
+	private function register_categories() {
+
+		foreach ( $this->categories as $slug => $args ) {
+			if ( WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $slug ) ) {
+				continue;
+			}
+
+			register_block_pattern_category( $slug, $args );
+		}
+
+	}
+
+	/**
+	 * Register Patterns.
+	 *
+	 * @return void
+	 */
+
+	private function register_patterns() {
+
+		foreach ( $this->patterns as $pattern ) {
+
+			$file = get_theme_file_path( '/patterns/' . $pattern . '.php' );
+
+			if ( ! is_file( $file ) ) {
+
+				continue;
+
+			}
+
+			register_block_pattern( 'blur/' . $pattern, require $file );
 		}
 	}
-
-	$block_patterns = array(
-		'hero-main',
-		'three-col-banner',
-		'recent-product',
-		'banner',
-		'top-rated-product',
-		'aboutus',
-		'service',
-		'primary-sidebar',
-		'sidebar-products',
-		'footer-default'
-	);
-
-	/**
-	 * Filters the theme block patterns.
-	 *
-	 * @since blur 1.0
-	 *
-	 * @param array $block_patterns List of block patterns by name.
-	 */
-	$block_patterns = apply_filters( 'blur_block_patterns', $block_patterns );
-
-	foreach ( $block_patterns as $block_pattern ) {
-		$pattern_file = get_theme_file_path( '/patterns/' . $block_pattern . '.php' );
-
-		register_block_pattern(
-			'blur/' . $block_pattern,
-			require $pattern_file
-		);
-	}
 }
-add_action( 'init', 'blur_register_block_patterns', 9 );
+
+new Block_Patterns();
