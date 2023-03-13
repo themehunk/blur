9b3be3f34bfe650d3c229ddaea1f19306a45a6f8
shivam kushwah <kushwah.shivam30@gmail.com>
2023-03-13 13:35:03 +0530

    save it

--- theme-option/build/index.asset.php
+++ theme-option/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'aed917d3f60148eaad60');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'f5b76f030a39dc48dd43');
